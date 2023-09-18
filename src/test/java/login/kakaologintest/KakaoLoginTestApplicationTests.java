0e8ae4422c21e200b2d1b721262dd7237cb281ef
mimddon <james990828@naver.com>
2023-09-19 00:29:55 +0900

    initial commit

---
+++ src/test/java/login/kakaologintest/KakaoLoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package login.kakaologintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoLoginTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
